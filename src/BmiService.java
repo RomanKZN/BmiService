a8057cb75f0d62f01b4cd8513f2ba88750b5a624
Roman Medvedev <romankazan88@gmail.com>
2022-05-18 19:45:39 +0300

    Commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
     public int calculate(int weight, double height) {
-        double bodyMassIndex= weight /height/height;
-
-
+        double bodyMassIndex = weight / height / height;
         return (int) bodyMassIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double height = 1.82;
-        int weight= 90;
-        int bodyMassIndex = service.calculate(weight,height);
+        int weight = 90;
+        int bodyMassIndex = service.calculate(weight, height);
         System.out.println(bodyMassIndex);
 
     }
